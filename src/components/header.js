b41a6cce029f3648226af79e38d2e7452cd6142d
Zolon Wilkins <zoloniv@gmail.com>
2020-02-29 11:11:25 -0800

    update title

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,13 @@ import React from "react"
 const Header = () => (
   <header className="flex flex-col md:flex-row justify-between text-3xl py-8 px-8 md:px-16">
     <span>
-      full stack developer @{" "}
-      <a className="underline wavy" href="https://mazsystems.com">
-        maz
+      software engineer @{" "}
+      <a
+        className="underline wavy"
+        href="https://lemonlight.com"
+        target="_blank"
+      >
+        lemonlight
       </a>
     </span>
     <a className="underline text-black wavy" href="mailto:zolon@dingo.rodeo">
